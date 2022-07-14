94def6eaa9d10fc649c4821de8c78a020f3ce23a
joaopedroAlldax <joao.costa@alldax.com>
2022-07-14 15:25:18 -0300

    Adicionando arquivo if-for

---
+++ Python/if-for.py
@@ -0,0 +1,19 @@
+for i in range(10):
+    if i > 5:
+        print(i)
+
+v1 = 22
+v2 = 30
+
+soma = v1+v2
+if soma > 55:
+    print(soma)
+else:
+    print("não é maior que 55")
+
+for n in range(100):
+    if n % 2 == 0:
+        print("Os numeros pares são: {} ".format(n))
+    else:
+        pass
+
