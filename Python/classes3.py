545e9d1bb2fbfcbc2e315bcb9b274e28197c88d1
joaopedroAlldax <joao.costa@alldax.com>
2022-07-20 17:59:30 -0300

    Atualização do arquivo classe3

--- Python/classes3.py
+++ Python/classes3.py
@@ -14,26 +14,32 @@ class Salario:
 
     def salario_mensal_bruto(self):
         self.salario_bruto = float(self.salario_hora) * float(self.horas_mes)
-        print("Seu salário bruto é de: {}".format(self.salario_bruto))
+        print("-----------")
+        print("Seu salário bruto é de: {}\n".format(self.salario_bruto))
 
     def desconto_inss(self):
         self.valor_inss = float(self.salario_bruto) * 0.11
-        print("Seu desconto INSS é no valor de: {}".format(self.valor_inss))
+        print("-----------")
+        print("Seu desconto INSS é no valor de: {}\n".format(self.valor_inss))
 
     def desconto_sindicato(self):
         self.valor_sindicato = float(self.salario_bruto) * 0.05
-        print("Seu desconto do Sindicado é no valor de: {}".format(self.valor_sindicato))
+        print("-----------")
+        print("Seu desconto do Sindicado é no valor de: {}\n".format(self.valor_sindicato))
 
     def descontos_somados(self):
         self.descontos = float(self.valor_inss) + float(self.valor_sindicato)
-        print("O total de descontos é no valor de: {}".format(self.descontos))
+        print("-----------")
+        print("O total de descontos é no valor de: {}\n".format(self.descontos))
 
     def salario_liquido(self):
         self.valor_salario_liquido = float(self.salario_bruto) - float(self.descontos)
+        print("-----------")
+        print("Seu salário liquido é de: {}\n".format(self.valor_salario_liquido))
         if self.valor_salario_liquido >= 2300:
-            print("Seu salário liquido é de: {}, e você deverá fazer seu imposto de renda".format(self.valor_salario_liquido))
+            print("Você deverá declarar seu IR")
         else:
-            pass
+            print("Você não precisa declarar seu IR")
 
 if __name__ == '__main__':
     salario_bruto = ''
