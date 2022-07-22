7c2d2cb3106d9bee5555bd71507da4558e0f1dd4
joaopedroAlldax <joao.costa@alldax.com>
2022-07-22 12:02:38 -0300

    adicionando o arquivo len

---
+++ Python/len.py
@@ -0,0 +1,14 @@
+"""Estudo sobre a função len, que consiste na contagem de caracteres existentes na string
+    o len leva em consideração os espaços do teclado"""
+
+
+
+usuario = input("qual é o seu nome? ")
+qnt_caracteres = len(usuario)
+print(qnt_caracteres)
+
+dicionario = {'Nome' : 'João', 'idade' : '19', 'cargo' : 'dev'}
+print(len(dicionario))
+
+lista = ['oi', 'como ce ta', 'eu', 'quero saber a cor da sua calcinha']
+print(len(lista))
